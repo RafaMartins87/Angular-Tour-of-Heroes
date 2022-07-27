35c859824a645952f2994cb120ca1c3bb2cdedea
rafael martins <rafael.martins87@outlook.com>
2022-07-27 00:03:25 -0300

    fix(27-07-22): corrigindo two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -13,9 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    NgModule,
-    NgModel
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
